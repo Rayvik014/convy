782505cfd3641d683f5fba0da53052ac2787cfa9
Rayvik014 <rayvik014@gmail.com>
2023-07-06 12:06:23 +0300

    make sure it works

--- controls/views.py
+++ controls/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from .models import Sector, Word, Progress
+from random import choice
+
+CHANCEONSTART = 50
 
 def sectors_list(request):
     sectors = Sector.objects.all()
@@ -18,3 +21,24 @@ def game(request):
 
 def index(request):
     return render(request, 'controls/Index.html')
+
+def button_plus_one(request):
+    word_ids_list = Word.objects.values_list('pk', flat=True)  #Make list with all ID's in Words
+    user_and_words = Progress.objects.values_list('user_id', 'word_id') #Make list with tuples [(user_id, word_id),]
+    users_words = set()
+    user_id = request.user.id
+    message = "Sorry, we haven't new words for you, please try later"
+    for x,y in user_and_words:              # Make set with word ID's in use of current user
+        if x == user_id:
+            users_words.add(y)
+    while users_words != {}:                # When I find the new word ID - I put the record to Progress base
+        random_pick = choice(word_ids_list)  #Choose random word ID
+        if random_pick not in users_words:
+            p = Progress(user_id=user_id, word_id=random_pick, chance=CHANCEONSTART)
+            p.save()
+            word_obj = Word.objects.get(pk=random_pick)
+            message = f"The Word {word_obj.lang1} / {word_obj.lang2} added"
+            break
+        else:
+            users_words.remove(random_pick)
+    return render(request, 'controls/Game.html', {"message":message})
