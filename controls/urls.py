79196884eacf0f82c65059afc04e401083726054
Rayvik014 <rayvik014@gmail.com>
2023-07-07 16:53:42 +0300

    make sure it works

--- controls/urls.py
+++ controls/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path("game", views.game, name="game"),
     path("", views.index, name="index"),
-    path("PlusOneWord", views.button_plus_one)
+    path("PlusOneWord", views.button_plus_one),
+    path("answer", views.answer)
 ]
--- controls/views.py
+++ controls/views.py
@@ -1,50 +1,72 @@
 from django.shortcuts import render
-from .models import Sector, Word, Progress
+from .models import Word, Progress
+from django.http import HttpResponseRedirect
+from .forms import AnswerForm
 import random
 
 def index(request):
     return render(request, 'controls/Index.html')
-
-def users_words(users_id):
-    """Returns a set with word ID's in use of current user"""
-    user_and_words = Progress.objects.values_list('user_id', 'word_id', 'chance')  # Make list with tuples [(user_id, word_id),]
-    users_words = []
-    users_chance = []
-    for x,y,z in user_and_words:
-        if x == users_id:
-            users_words.append(y)              # Make list with word ID's in use of current user
-            users_chance.append(z)             # Make list with chances of words in use of current user
-    return users_words, users_chance
 
 def button_plus_one(request):
     """This script for button adds one new learning word for user.
     It takes random word from Word base, checks is this word unique for User,
     and makes a record in Progress Database with User ID, Word ID, and Chance
     """
-    word_ids_list = Word.objects.values_list('pk', flat=True)   #Make QuerySet with all ID's in Words
-    users_id = request.user.id
+    # QuerySet of All words in Base:
+    word_ids_list = Word.objects.values_list('pk', flat=True)
+    # QuerySet of All words of current User:
+    user_words = Progress.objects.filter(user_id=request.user.id).values_list('word_id', flat=True)
     message = "Sorry, we haven't new words for you, please try later"
-    excludes = set()
-    user_words = users_words(users_id)[0]   #Uses users_words, not users_words_chance
-    while len(word_ids_list) > 0 :                # When I find the new word ID - I put the record to Progress base
-        random_pick = random.choice(word_ids_list)  #Choose random word ID
+    excludes_words = set()
+    while len(word_ids_list) > 0:
+        random_pick = random.choice(word_ids_list)                      #Choose random word ID
         if random_pick not in user_words:
-            p = Progress(user_id=users_id, word_id=random_pick)  #Write the new string in database
+            p = Progress(user_id=request.user.id, word_id=random_pick)  #Write the new string in database
             p.save()
             word_obj = Word.objects.get(pk=random_pick)
             message = f"The Word {word_obj.lang1}/{word_obj.lang2} added"
             break
         else:
-            user_words.remove(random_pick)
-            excludes.add(random_pick)
-            word_ids_list = word_ids_list.exclude(pk__in=excludes) # excludes already dropped picks
-    return render(request, 'controls/Game.html', {"message":message})
+            excludes_words.add(random_pick)                              # excludes already dropped picks
+            word_ids_list = word_ids_list.exclude(pk__in=excludes_words)
+            user_words = user_words.exclude(pk__in=excludes_words)
+    return render(request, 'controls/Game.html', {'message':message})
 
 def game(request):
     """Function is offer the random Word from User's base for translation, structure of letters and spaces.
     """
-    users_id = request.user.id
-    user_words, user_chance = users_words(users_id)
-    random_pick = random.choices(user_words, weights=user_chance)
-    print(random_pick)
-    return render(request, 'controls/Game.html', {'random_pick':random_pick})
+    user_words = Progress.objects.filter(user_id=request.user.id).values_list('word_id', flat=True)
+    word_chances = Progress.objects.filter(user_id=request.user.id).values_list('chance', flat=True)
+    try:
+        random_pick = random.choices(user_words, weights=word_chances)[0]
+        # Zero here because random.choices returns a list with one value instead one integer
+    except IndexError:
+        random_pick = "You have not any words"
+    lang = random.choice((1, 2))
+    offered_object = Word.objects.get(id=random_pick)
+    offered_word = offered_object.lang1 if lang == 1 else offered_object.lang2
+    offered_sector = offered_object.sector
+    offered_structure = make_word_structure(offered_object.lang2) if lang == 1 else make_word_structure(offered_object.lang1)
+    return render(request, 'controls/Game.html', {'offered_word':offered_word,
+                                                  'offered_sector':offered_sector,
+                                                  'offered_structure':offered_structure})
+
+def make_word_structure(string):
+    structure = ""
+    for char in string:
+        if char != " ":
+            structure += "_"
+        else:
+            structure += char
+    return structure
+
+def answer(request):
+    if request.method == 'POST':
+        form = AnswerForm(request.POST)
+        if form.is_valid():
+            the_answer = form.cleaned_data['answer']
+            print(the_answer)
+        return HttpResponseRedirect('game')
+    else:
+        form = AnswerForm()
+    return render(request, 'game', {'form': form})
